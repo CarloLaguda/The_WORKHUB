0e3da4762424e3ebe50fd3f73431ca288e34f346
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-09-27 22:45:55 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,6 @@ sudo apt update
 sudo apt install mariadb-server -y
 sudo service mariadb start
 sudo mariadb
-pip install mysql-connector-python 
-pip install pandas
+pip install -r .devcontainer/requirements.txt
 sudo npm install -g @angular/cli
 python3 server/main.py
