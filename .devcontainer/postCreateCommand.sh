fd4d667eaf92ef143afa6796775c9b9804f27953
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-09-27 23:13:54 +0200

    Create postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install mariadb-server -y
+sudo service mariadb start
+sudo mariadb
+pip install -r .devcontainer/requirements.txt
+sudo npm install -g @angular/cli
+python3 WorkHub/server/main.py
