e5bacb1e8690b21a9e1b0b3caff32cd3691f485c
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-09-28 21:47:32 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,4 +4,5 @@ sudo service mariadb start
 sudo mariadb
 pip install -r .devcontainer/requirements.txt
 sudo npm install -g @angular/cli
-python3 WorkHub/server/main.py
+python3 WorkHub/Server/main.py
+python3 WorkHub/Server/routes/users_route.py
