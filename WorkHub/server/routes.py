28fba6f4970d5742d94ace60341ce01bb0cb58ba
Iljas Toci <159021859+QuckquackMF@users.noreply.github.com>
2025-09-29 10:20:07 +0000

    toci

---
+++ WorkHub/server/routes.py
@@ -0,0 +1,4 @@
+io sto faccendo il backend, nel frontend ce la registrazione/login, searchbar per key word, filters for user age, skills, language,
+ country. when we click on a user it shows their profile which has name surname, age and skills. then there is projects where we see
+  project data and creator and who is participating, users can join a project so the data gets updated and all the rest that comes
+   with it. give the necessary routs
