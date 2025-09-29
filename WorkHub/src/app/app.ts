543518834319b3983832e6e44925687ea83deb08
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2025-09-29 10:48:09 +0000

    home layout v1

--- WorkHub/src/app/app.ts
+++ WorkHub/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('WorkHub');
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
