22bbcb3fab2ec7bcfdcbb5a82cbe9fedab4738a2
wheelchaired_potato <159021761+LaPatataSullaSedia@users.noreply.github.com>
2025-10-06 10:47:50 +0000

    register layout v2

--- WorkHub/src/app/registrazione/registrazione.ts
+++ WorkHub/src/app/registrazione/registrazione.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registrazione',
   imports: [RouterLink],
   templateUrl: './registrazione.html',
-  styleUrl: './registrazione.css'
+  styleUrls: ['./registrazione.css']
 })
 export class Registrazione {
-
+ 
 }
