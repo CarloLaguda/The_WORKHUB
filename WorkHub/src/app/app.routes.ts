6b3047e80d58fe0b72b470c70e00d7d7c23ae852
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2025-10-08 10:32:36 +0000

    Login layout v1

--- WorkHub/src/app/app.routes.ts
+++ WorkHub/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Registrazione } from './registrazione/registrazione';
 import { App } from './app';
 import { Home } from './home/home';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     { path: 'signUp', component: Registrazione },
+    { path: 'signIn', component: Login },
     { path: '', component: Home },
 ];
