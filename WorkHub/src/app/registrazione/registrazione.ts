d07260571374ea573ae60df2e258c53457ebf30c
wheelchaired_potato <159021761+LaPatataSullaSedia@users.noreply.github.com>
2025-10-08 21:34:56 +0000

    grafica registrazione

--- WorkHub/src/app/registrazione/registrazione.ts
+++ WorkHub/src/app/registrazione/registrazione.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormArray, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
@@ -9,5 +9,13 @@ import { FormArray, FormBuilder, FormGroup, Validators, AbstractControl } from '
   styleUrls: ['./registrazione.css']
 })
 export class Registrazione {
- 
+ ngOnInit(): void {
+    // Blocca lo scroll
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy(): void {
+    // Ripristina lo scroll quando esci dal componente
+    document.body.style.overflow = 'auto';
+  }
 }
